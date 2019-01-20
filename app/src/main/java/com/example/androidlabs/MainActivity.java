bbde9ff7f067405bb8bfe649b544da62368dbc76
Yan Qu <ylxz333@gmail.com>
2019-01-19 23:17:50 -0500

    Initial commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //the first function is onCreate, it is equal to java main method
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
