01532bc2f69ee4beb640f228a95d704c767cf9c6
Yan Qu <ylxz333@gmail.com>
2019-02-11 09:25:48 -0500

    fixed commit

---
+++ app/src/main/java/com/example/androidlabs/Message.java
@@ -0,0 +1,32 @@
+package com.example.androidlabs;
+
+import android.app.Activity;
+
+public class Message{
+    private String message;
+    private Role role;
+
+    Message(String message, Role role){
+        this.role = role;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public Role getRole(){
+        return role;
+    }
+
+    public void setRole(Role role){
+        this.role = role;
+    }
+
+    enum Role{
+        SEND, RECIEVE
+    }
+}
